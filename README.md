9303d99c936b81f5c756cb3b2dec6594e8938108
arnaud <f@nanaud.fr>
2026-08-24 21:15:17 +0200

    Initial PWA wrapper

---
+++ README.md
@@ -0,0 +1,5 @@
+# BnF Access PWA
+
+Progressive Web App wrapper for [BnF Access](https://bnfaccess.fr/).
+
+The main site stays a normal website on `bnfaccess.fr`. This repository hosts the installable application shell intended for `app.bnfaccess.fr`.
---
+++ sw.js
@@ -0,0 +1,63 @@
+const cacheName = "bnf-access-pwa-v1";
+const appShellUrls = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./pwa.js",
+  "./bnf-access-icon.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(appShellUrls))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.filter((key) => key.startsWith("bnf-access-pwa-") && key !== cacheName).map((key) => caches.delete(key)))
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  const requestUrl = new URL(request.url);
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  if (request.mode === "navigate") {
+    event.respondWith(fetch(request).catch(() => caches.match("./index.html")));
+    return;
+  }
+
+  event.respondWith(cacheFirst(request));
+});
+
+async function cacheFirst(request) {
+  const cached = await caches.match(request);
+  if (cached) {
+    return cached;
+  }
+
+  const response = await fetch(request);
+  if (response.ok) {
+    const cache = await caches.open(cacheName);
+    cache.put(request, response.clone());
+  }
+
+  return response;
+}
